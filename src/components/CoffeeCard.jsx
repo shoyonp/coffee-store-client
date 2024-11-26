c031f613ad54b6a3321cbe2ef10c956db52a4612
shoyonp <shoyonkumar143@gmail.com>
2024-11-27 05:51:13 +0600

    all done

---
+++ src/components/CoffeeCard.jsx
@@ -0,0 +1,72 @@
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
+const CoffeeCard = ({ coffee, coffees, setCoffees }) => {
+  const { _id, name, quantity, supplier, category, details, taste, photo } =
+    coffee;
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "delete",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your coffee has been deleted.",
+                icon: "success",
+              });
+              const remaining = coffees.filter(cof => cof._id !== _id);
+              setCoffees(remaining);
+            }
+          });
+        console.log("delete confirm");
+      }
+    });
+  };
+
+  return (
+    <div className="card card-side bg-[#F5F4F1] hover:shadow-xl">
+      <figure>
+        <img src={photo} alt="Movie" />
+      </figure>
+      <div className="flex items-center justify-between w-full pr-4">
+        <div>
+          <h2 className="card-title">Name: {name}</h2>
+          <p>{quantity}</p>
+          <p>{supplier}</p>
+          <p>{taste}</p>
+        </div>
+        <div className="card-actions justify-end">
+          <div className="join join-vertical space-y-3 my-auto">
+            <button className="btn bg-[#D2B48C] text-white">VIEW</button>
+            <Link to={`updateCoffee/${_id}`}>
+              <button className="btn bg-[#3C393B] text-white">EDIT</button>
+            </Link>
+            <button
+              onClick={() => handleDelete(_id)}
+              className="btn bg-[#EA4744] text-white"
+            >
+              X
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoffeeCard;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AddCoffee from "./components/AddCoffee.jsx";
+import UpdateCoffee from "./components/UpdateCoffee.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    loader: () => fetch("http://localhost:5000/coffee"),
+  },
+  {
+    path: "addCoffee",
+    element: <AddCoffee></AddCoffee>,
+  },
+  {
+    path: "updateCoffee/:id",
+    element: <UpdateCoffee></UpdateCoffee>,
+    loader: ({ params }) => fetch(`http://localhost:5000/coffee/${params.id}`),
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
