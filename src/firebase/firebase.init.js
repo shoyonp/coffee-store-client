814ac75ac00e77940f6b03447d3505011ebbd662
shoyonp <shoyonkumar143@gmail.com>
2024-11-30 02:05:16 +0600

    done

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -17,3 +17,18 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
+
+/**
+ * 1. firebase-tools (one time)
+ * 2. firebase login (one time)
+ * 3. one time for eact project : firebase init
+ * 4. what you want to do: select hosting
+ * 5. select a project (already have one created in firebase console)
+ * 6. watt will be your public ---- dist 
+ * 7. single page?: y
+ * --------------------
+ * ### each time you want to deploy
+ * 8. npm run build
+ * 9. firebase deploy
+ */
